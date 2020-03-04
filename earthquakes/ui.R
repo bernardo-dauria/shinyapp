bc06601abd7b8bceb19e5a04842af16175b14d89
codefluence <codefluence@outlook.com>
2020-03-04 23:42:25 +0100

    grid + brushing events added (messy)

--- earthquakes/ui.R
+++ earthquakes/ui.R
@@ -8,19 +8,18 @@ library(shinyjs)
 
 data = read_csv("earthquakes/data.csv")
 
-data$Magnitude = data$Magnitude * 1e5
-data$q <- with(data, cut(Magnitude, quantile(Magnitude)))
-levels(data) <- paste(c("1st", "2nd", "3rd", "4th", "5th"), "Quantile")
-data$q <- as.ordered(data$q)
+data = data[,c(1:6,9)]
+data$Magnitude = data$Magnitude
 
+#data$q <- with(data, cut(Magnitude, quantile(Magnitude)))
+#levels(data) <- paste(c("1st", "2nd", "3rd", "4th", "5th"), "Quantile")
+#data$q <- as.ordered(data$q)
 
 data$Year = as.numeric(format(strptime(as.character(data$Date), "%m/%d/%Y"), "%Y"))
 data = drop_na(data, c("Year"))
 
-table(data$Year)
-
-plot(density(as.numeric(data$Year)))
-range(data$Year)
+#table(data$Year)
+#plot(density(as.numeric(data$Year)))
 
 
 ###############################################################
@@ -47,17 +46,33 @@ ui = shinyUI(fluidPage(
         )
     ),
     
-    sliderInput("slider", "Range:",
+    fluidRow(
+        column(width = 9,
+               wellPanel(width = 9,
+                         h4("Points selected by brushing, with brushedPoints():"),
+                         DT::dataTableOutput("plot_brushed_points")
+               )
+        ),
+        column(width = 3,
+               verbatimTextOutput("plot_brushinfo")
+        )
+    ),
+    
+    sliderInput("slider", "Time Span:",
                 min = min(data$Year), max = max(data$Year),
                 value = c(2000,2016)),
     
+    sliderInput("slider2", "Magnitude Range:",
+                min = min(data$Magnitude), max = max(data$Magnitude),
+                value = c(7,9.1)),
+
     plotlyOutput("plot"),
     
-    verbatimTextOutput("event"),
+    verbatimTextOutput("event")
     
-    fluidRow(
-        column(4, verbatimTextOutput("aaa"))
-    )
+    ##fluidRow(
+    #    column(4, verbatimTextOutput("aaa"))
+    #)
 ))
 
 
@@ -65,9 +80,12 @@ server <- function(input, output) {
     
     dataInput = reactive({
         
-        mmin = min(as.numeric(input$slider))
-        mmax = max(as.numeric(input$slider))
-        copy = filter(data, Year>= mmin & Year <= mmax)
+        copy = filter(data, Year>= min(as.numeric(input$slider)) & Year <= max(as.numeric(input$slider)))
+        copy = filter(copy, Magnitude>= min(as.numeric(input$slider2)) & Magnitude <= max(as.numeric(input$slider2)))
+        
+        #copy = rbind(copy,list(Magnitude=min(data$Magnitude)))
+        #copy = rbind(copy,list(Magnitude=max(data$Magnitude)))
+
         copy
     })
     
@@ -75,7 +93,7 @@ server <- function(input, output) {
     output$plot <- renderPlotly({
         
         df = dataInput()
-        
+
         g <- list(
             #scope = 'usa',
             #projection = list(type = 'albers usa'),
@@ -92,18 +110,62 @@ server <- function(input, output) {
         #locationmode = 'USA-states', 
         p <- plot_geo(df, sizes = c(1, 250), width = 1200, height = 700) %>%
             add_markers(
-                x = ~Longitude, y = ~Latitude, size = ~Magnitude, color = ~q, hoverinfo = "text",
-                text = ~paste(df$name, "<br />", df$Magnitude/1e6, " year is:", nrow(df))
+                x = ~Longitude, y = ~Latitude, size = ~Magnitude, color = ~Depth, hoverinfo = "text", sizes = range(exp(df$Magnitude))/40,
+                text = ~paste(df$name, "<br /> Magnitude:", df$Magnitude, "<br />", df$Year, "<br />", df$Latitude, "<br />", df$Longitude),
+                brush = ~brushOpts("plot_brush")
             ) %>%
-            layout(title = '2014 US city populations<br>(Click legend to toggle)', geo = g)
+            layout(title = paste(nrow(df),"earquakes"), geo = g)
     })
-    
-    output$aaa <- renderPrint({ input$slider })
-    
+
     output$event <- renderPrint({
         d <- event_data("plotly_hover")
         if (is.null(d)) "Select a point for more detail." else d
     })
+
+    # listen to the brushing event and draw a
+    # rect shape that mimics the brush
+    observe({
+        brush <- event_data("plotly_brushing")
+        
+        if (!is.null(brush) && !is.na(brush) && !is.na(brush$geo) && nrow(brush$geo) == 2)
+        {
+            print(brush$geo)
+            
+            copy = filter(data, Year>= min(as.numeric(input$slider)) & Year <= max(as.numeric(input$slider)))
+            copy = filter(copy, Magnitude>= min(as.numeric(input$slider2)) & Magnitude <= max(as.numeric(input$slider2)))
+            copy = filter(copy, Longitude>= brush$geo[1,1] & Longitude <= brush$geo[2,1])
+            copy = filter(copy, Latitude>= brush$geo[2,2] & Latitude <= brush$geo[1,2])
+            
+            #res <- brushedPoints(dataInput(), brush)
+            
+            #rint("res")
+            print(paste("len",nrow(copy)))
+            datatable(copy)
+        }
+    })
+    
+    
+    output$plot_brushed_points <- DT::renderDataTable({
+        
+        brush <- event_data("plotly_brushing")
+        
+        if (!is.null(brush) && !is.na(brush) && !is.na(brush$geo) && nrow(brush$geo) == 2)
+        {
+            print(brush$geo)
+            
+            copy = filter(data, Year>= min(as.numeric(input$slider)) & Year <= max(as.numeric(input$slider)))
+            copy = filter(copy, Magnitude>= min(as.numeric(input$slider2)) & Magnitude <= max(as.numeric(input$slider2)))
+            copy = filter(copy, Longitude>= brush$geo[1,1] & Longitude <= brush$geo[2,1])
+            copy = filter(copy, Latitude>= brush$geo[2,2] & Latitude <= brush$geo[1,2])
+            
+            #res <- brushedPoints(dataInput(), brush)
+            
+            #rint("res")
+            print(paste("len",nrow(copy)))
+            datatable(copy)
+        }
+    })
+    
 }
 
 
