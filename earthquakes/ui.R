d3af6cbe8e1a4bc71bfd5052064b463734cf0369
codefluence <codefluence@outlook.com>
2020-03-04 16:59:40 +0100

    temporary messy stuff

--- earthquakes/ui.R
+++ earthquakes/ui.R
@@ -2,9 +2,31 @@ rm(list = ls())
 library(shiny)
 library(plotly)
 library(tidyverse)
+library(shinyjs)
 
 
-shinyUI = shinyUI(fluidPage(
+
+data = read_csv("earthquakes/data.csv")
+
+data$Magnitude = data$Magnitude * 1e5
+data$q <- with(data, cut(Magnitude, quantile(Magnitude)))
+levels(data) <- paste(c("1st", "2nd", "3rd", "4th", "5th"), "Quantile")
+data$q <- as.ordered(data$q)
+
+
+data$Year = as.numeric(format(strptime(as.character(data$Date), "%m/%d/%Y"), "%Y"))
+data = drop_na(data, c("Year"))
+
+table(data$Year)
+
+plot(density(as.numeric(data$Year)))
+range(data$Year)
+
+
+###############################################################
+
+
+ui = shinyUI(fluidPage(
 
     # Application title
     titlePanel("Old Faithful Geyser Data"),
@@ -21,91 +43,63 @@ shinyUI = shinyUI(fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("distPlot")
+            
         )
+    ),
+    
+    sliderInput("slider", "Range:",
+                min = min(data$Year), max = max(data$Year),
+                value = c(2000,2016)),
+    
+    plotlyOutput("plot"),
+    
+    verbatimTextOutput("event"),
+    
+    fluidRow(
+        column(4, verbatimTextOutput("aaa"))
     )
 ))
-
-ui <- fluidPage(
-    plotlyOutput("plot"),
-    verbatimTextOutput("event")
-)
-
-
-
-shinyServer = shinyServer(function(input, output) {
-    
-    output$plot <- renderPlotly({
-        
-        df <- read.csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_us_cities.csv')
-        
-        df$q <- with(df, cut(pop, quantile(pop)))
-        levels(df$q) <- paste(c("1st", "2nd", "3rd", "4th", "5th"), "Quantile")
-        df$q <- as.ordered(df$q)
-        
-        g <- list(
-            scope = 'usa',
-            projection = list(type = 'albers usa'),
-            showland = TRUE,
-            landcolor = toRGB("gray85"),
-            subunitwidth = 1,
-            countrywidth = 1,
-            subunitcolor = toRGB("white"),
-            countrycolor = toRGB("white")
-        )
-        
-        p <- plot_geo(df, locationmode = 'USA-states', sizes = c(1, 250)) %>%
-            add_markers(
-                x = ~lon, y = ~lat, size = ~pop, color = ~q, hoverinfo = "text",
-                text = ~paste(df$name, "<br />", df$pop/1e6, " million")
-            ) %>%
-            layout(title = '2014 US city populations<br>(Click legend to toggle)', geo = g)
-        
-        p
-    })
-    
-    
-})
 
 
 server <- function(input, output) {
     
-    df2 <- read.csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_us_cities.csv')
-    
-    df = read_csv("earthquakes/data.csv")
-    df$Magnitude = df$Magnitude * 1e5
-    
-    df$q <- with(df, cut(Magnitude, quantile(Magnitude)))
-    levels(df$q) <- paste(c("1st", "2nd", "3rd", "4th", "5th"), "Quantile")
-    df$q <- as.ordered(df$q)
-    
-    g <- list(
-        #scope = 'usa',
-        #projection = list(type = 'albers usa'),
-        showland = TRUE,
-        landcolor = toRGB("gray85"),
-        subunitwidth = 1,
-        countrywidth = 1,
-        list(lonaxis = list(range = c(-30, 30))),
-        list(lataxis = list(range = c(-30, 30))),
-        subunitcolor = toRGB("white"),
-        countrycolor = toRGB("white")
-    )
-    
-    #locationmode = 'USA-states', 
-    p <- plot_geo(df, sizes = c(1, 250), width = 1200, height = 700) %>%
-         add_markers(
-            x = ~Longitude, y = ~Latitude, size = ~Magnitude, color = ~q, hoverinfo = "text",
-            text = ~paste(df$name, "<br />", df$Magnitude/1e6, " million")
-        ) %>%
-        layout(title = '2014 US city populations<br>(Click legend to toggle)', geo = g)
-    
+    dataInput = reactive({
+        
+        mmin = min(as.numeric(input$slider))
+        mmax = max(as.numeric(input$slider))
+        copy = filter(data, Year>= mmin & Year <= mmax)
+        copy
+    })
     
     # renderPlotly() also understands ggplot2 objects!
     output$plot <- renderPlotly({
-        p$elementId <- NULL
-        p
+        
+        df = dataInput()
+        
+        g <- list(
+            #scope = 'usa',
+            #projection = list(type = 'albers usa'),
+            showland = TRUE,
+            landcolor = toRGB("gray85"),
+            subunitwidth = 1,
+            countrywidth = 1,
+            list(lonaxis = list(range = c(-30, 30))),
+            list(lataxis = list(range = c(-30, 30))),
+            subunitcolor = toRGB("white"),
+            countrycolor = toRGB("white")
+        )
+        
+        #locationmode = 'USA-states', 
+        p <- plot_geo(df, sizes = c(1, 250), width = 1200, height = 700) %>%
+            add_markers(
+                x = ~Longitude, y = ~Latitude, size = ~Magnitude, color = ~q, hoverinfo = "text",
+                text = ~paste(df$name, "<br />", df$Magnitude/1e6, " year is:", nrow(df))
+            ) %>%
+            layout(title = '2014 US city populations<br>(Click legend to toggle)', geo = g)
     })
+    
+    output$aaa <- renderPrint({ input$slider })
+    
     output$event <- renderPrint({
         d <- event_data("plotly_hover")
         if (is.null(d)) "Select a point for more detail." else d
@@ -115,8 +109,6 @@ server <- function(input, output) {
 
 ##########
 
-
-data = read_csv("earthquakes/data.csv")
 
 shinyApp(ui = ui, server = server, options = list(height = 830, width = 830))
 
