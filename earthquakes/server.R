04155c7c256a2bc8601928595d749c9409ca0bf6
codefluence <codefluence@outlook.com>
2020-03-11 22:57:25 +0100

    moving data preprocessing to the server file

--- earthquakes/server.R
+++ earthquakes/server.R
@@ -1,6 +1,23 @@
 
 server <- function(input, output) {
     
+    
+    ### data preprocessing ###
+    
+    data = read_csv("data.csv")
+    
+    data = data[,c(1:6,9)]
+    data = filter(data, Type == "Earthquake") #Only earthquake are relevant in this app
+    data = data[,-5]
+    
+    data$Longitude = as.numeric(data$Longitude)
+    data$Latitude = as.numeric(data$Latitude)
+    
+    data$Year = as.numeric(format(strptime(as.character(data$Date), "%m/%d/%Y"), "%Y"))
+    data$ScaledMagnitude = rescale(data$Magnitude, to=c(5,100))
+    data = drop_na(data, c("Year"))
+    
+    
     dataInput = reactive({
         
         copy = filter(data, Year>= min(as.numeric(input$slider)) & Year <= max(as.numeric(input$slider)))
